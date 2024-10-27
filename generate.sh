2c304df352b6d49a52d00bd26dff8719c2494b86
asepopov <asepopov@avito.ru>
2024-10-27 14:55:44 +0300

    add method for rpc

--- generate.sh
+++ generate.sh
@@ -1,3 +1,9 @@
 protoc  --go_out=./  \
         --go-grpc_out=./ \
+        notification.proto
+
+protoc --doc_out=. --doc_opt=markdown,README.md ./notification.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
         new_user_invite.proto
